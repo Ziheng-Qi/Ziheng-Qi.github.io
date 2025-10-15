7962e42d426191f510be8fd049e3af966fc319bf
Ziheng-Qi <123517769+Ziheng-Qi@users.noreply.github.com>
2025-10-15 14:49:13 +0000

    Deploying to gh-pages from @ Ziheng-Qi/Ziheng-Qi.github.io@5feaee93b564f6c393cb834c558fe0004384e727 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,12 +423,12 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_2.md/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-i-have-created-my-personal-website-sparkles",
-          title: 'I have created my personal website! :sparkles:',
+          section: "News",},{id: "news-i-have-created-my-personal-website",
+          title: 'I have created my personal website! ✨',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -480,35 +480,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%69%68%65%6E%67%71%32@%69%6C%6C%69%6E%6F%69%73.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Ziheng-Qi", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/ziheng-qi", "_blank");
         },
       },{
       id: 'light-theme',
